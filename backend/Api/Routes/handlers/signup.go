3c62156d981f800ba14933fa158ec3048464f85e
AbbasRizvi3 <bassiiroti@gmail.com>
2026-01-06 18:25:08 +0500

    duplicate user fix

--- backend/Api/Routes/handlers/signup.go
+++ backend/Api/Routes/handlers/signup.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"net/http"
+
 	config "github.com/AbbasRizvi3/GoLangReactApp/Config"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type SignupInput struct {
@@ -15,6 +18,14 @@ func SignupHandler(c *gin.Context) {
 	var input SignupInput
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	var existingUser bson.M
+	err := config.Client.Database("appdb").Collection("users").
+		FindOne(c, bson.M{"email": input.Email}).Decode(&existingUser)
+
+	if err == nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "User already exists"})
 		return
 	}
 	config.Client.Database("appdb").Collection("users").InsertOne(c, input)
